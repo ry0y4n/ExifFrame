d2d16ef3e859a4ed87663c811abede2a084249e4
momosuke <rusk.ryoy4n@gmail.com>
2023-07-13 20:44:09 +0900

    add: feature that can install as pwa

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'ExifFrame-cache-v1';
+const urlsToCache = [
+    '/index.html',
+    '/index.css',
+    '/script.js',
+    '/terms.html',
+    '/terms.css',
+    '/global.css'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
