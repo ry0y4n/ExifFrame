928756748d1b275715403dcade68ab7715181167
momosuke <rusk.ryoy4n@gmail.com>
2023-09-04 16:27:45 +0900

    add: prettier to enable general linter setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,30 @@
 /* global module */
 
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'globals': {
-    'EXIF': 'readonly'
+  globals: {
+    EXIF: 'readonly',
   },
-  'rules': {
+  rules: {
     'no-control-regex': 'off', // 正規表現で制御文字を使用可能にする
-    'semi': ['error', 'always'], // セミコロンを強制
-    'semi-spacing': ['error', {'after': true, 'before': false}],
+    semi: ['error', 'always'], // セミコロンを強制
+    'semi-spacing': ['error', { after: true, before: false }],
     'semi-style': ['error', 'last'],
     'no-extra-semi': 'error',
     'no-unexpected-multiline': 'error',
     'no-unreachable': 'error',
-    'indent': ['error', 2], // インデントはスペース2つ
-    'quotes': ['error', 'single'], // シングルクォートのみ
+    indent: ['error', 2], // インデントはスペース2つ
+    quotes: ['error', 'single'], // シングルクォートのみ
     'brace-style': ['error', '1tbs'], // ブレースは1tbs
-    'multiline-ternary': ['error', 'always'], // 三項演算子は複数行に分ける
-    'newline-per-chained-call': ['error', {'ignoreChainWithDepth': 2}], // メソッドチェーンの深さは2つまで
-    'no-multiple-empty-lines': ['error', {'max': 1}], // 空行は1行まで
-  }
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }], // メソッドチェーンの深さは2つまで
+    'no-multiple-empty-lines': ['error', { max: 1 }], // 空行は1行まで
+  },
 };
